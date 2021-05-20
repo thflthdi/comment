1fec88b25d1533225c2d34307dcd00a0e0f73522
thflthdi <thflthdi@gmail.com>
2021-05-20 14:39:10 +0900

    add: delete comment 기능 구현

--- comment_app/js/main.js
+++ comment_app/js/main.js
@@ -25,34 +25,59 @@ if(isLogin()){
 }
 
 const login = () =>{
-    const id = 1;
-    window.sessionStorage.setItem('id',1);
+    let userId = '';
+    const choice = document.getElementsByName('choiceUser');
+    choice.forEach((user)=>{if(user.checked){userId=user.id}})
+    window.sessionStorage.setItem('id',userId);
     document.getElementsByClassName("login")[0].style.display = "none";
     document.getElementsByClassName("logout")[0].style.display = "block";
     modal.style.display = "none";
+    document.getElementsByClassName('comment')[0].innerHTML = getComment();
 }
 
 const logout = () =>{
-    const id = 1;
     window.sessionStorage.removeItem('id');
     document.getElementsByClassName("logout")[0].style.display = "none";
     document.getElementsByClassName("login")[0].style.display = "block";
     modal.style.display = "none";
+    document.getElementsByClassName('comment')[0].innerHTML = getComment();
+}
+
+const getComment = () => {
+    const now = localStorage.getItem('num')
+    let result = ''
+    for(let i=1; i<Number(now)+1;i++){
+
+    try{let user = JSON.parse(localStorage.getItem(i))
+        // let newDiv = document.createElement("div");
+        // let newContent = document.createTextNode(`${user.text}`);
+        // newDiv.appendChild(newContent)
+        inner = `<div class="comment-user"> user:${user.user}</div>
+                    <div class="comment-text">text:${user.text} </div>
+                    <div class="up" onclick="thumbUpDown(1)">좋아요
+                        <span class="up_num">30</span>
+                    </div>
+                    <div class="down" onclick="thumbUpDown(2)">싫어요</div>`
+
+        if(user.user===isLogin()){
+            inner=inner+`<button class="com-mod" onclick="comMod(${i})">수정</button>
+            <button class="com-del" id="${i}" onclick="comDel(id)">삭제</button>`
+        }
+        inner=inner+`<hr/`
+        result = result+inner
+    }catch(e){
+        // console.log(e)
+        }
+    }
+        return result
 }
 
 if(!localStorage.getItem('num')){
     localStorage.setItem('num',0);
 }else{
-    const now = localStorage.getItem('num')
-    let result = ''
-    console.log(now)
-    for(let i=1; i<Number(now)+1;i++){
-        const user = JSON.parse(localStorage.getItem(i))
-        inner = `<div class="comment-user"> user:${user.user}</div><div class="comment-text">text:${user.text} </div>`
-        result = result+inner
-    }
-    document.getElementsByClassName('comment')[0].innerHTML = result;
+    document.getElementsByClassName('comment')[0].innerHTML = getComment();
 }
+
 //댓글 생성
 const creComment = () => {
     if(isLogin()){
@@ -64,47 +89,29 @@ const creComment = () => {
             const now = Number(num)+1
             localStorage.setItem(now,JSON.stringify({user:isLogin(),text:comment}))
             localStorage.setItem('num',now)
-
-            let result = '';
-            for(let i=1; i<now+1;i++){
-                const user = JSON.parse(localStorage.getItem(i))
-                inner = `<div class="comment-user"> user:${user.user}</div><div class="comment-text">text:${user.text} </div>`
-                result = result+inner
-            }
-            document.getElementsByClassName('comment')[0].innerHTML = result;
+        document.getElementsByClassName('comment')[0].innerHTML = getComment();
         }
     }else{
         openModal();
     }
 }
 
-// const creComment = () => {
-//    data= [{   title : "damaged JEAN",
-//     content : "18f/w hot item",
-//     price : 75000
-//     },
-//     {
-//     title: "long-sleeve T-shirt",
-//     content: "2019s/s new!",
-//     price: 25000
-//     }];
-//     var html = "<li><h4>{title}</h4><p>{content}</p><div>{price}</div></li>";
-
-//     data.forEach( (el) => {
-//     let result = html.replace("{title}",el.title)
-//          .replace("{content}", el.content)
-//            .replace("{price}", el.price);
-//     el.innerHTML += result;
-//         document.getElementsByClassName('comment')[0].innerHTML = result
-// })
-// }
+const comDel = (comId) => {
+    try{const getInfo = JSON.parse(localStorage.getItem(comId));
+        if(getInfo && getInfo.user === isLogin()){
+            localStorage.removeItem(comId)
+            document.getElementsByClassName('comment')[0].innerHTML = getComment();
+        }else{
+            console.log('delete error')
+        }
+    }catch(e){console.log(e)
+        console.log('Delete Error : cant get Info',e)}
+    
+}
 
 const thumbUpDown = (value) => {
-    console.log(value)
     if(value===1){
         let num = document.getElementsByClassName('up_num')[0].outerHTML;
-        console.log(typeof(num))
-        console.log(Number(num)+1)
         document.getElementsByClassName('up_num')
     }
 }
